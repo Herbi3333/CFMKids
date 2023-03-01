46480823fcedd8d8b8d30b4f62e7a338932ca1ee
Herbi3333 <hermat3333@gmail.com>
2023-03-01 10:40:28 -0700

    Update signUpBtn_test.dart

--- test/signUpBtn_test.dart
+++ test/signUpBtn_test.dart
@@ -10,19 +10,29 @@ import 'package:cfmkids/main.dart';
 // TODO: Add test cases to verify the app against different screen sizes and orientations
 
 void main() {
-  testWidgets('Login button test', (WidgetTester tester) async {
+  testWidgets('Sign Up button test', (WidgetTester tester) async {
     // Build the widget
-    await tester.pumpWidget(const MaterialApp(home: HomePage()));
+    await tester.pumpWidget(MaterialApp(
+      home: HomePage(
+        body: signupBtn(
+          onPressed: () {
+            print('Sign up Button tapped!');
+          },
+          child: Text('Button'),
+        ),
+      ),
+    ));
 
-    //Verify that the Login button is displayed
-    final loginBtn = find.byKey(const Key('loginBtn'));
-    expect(loginBtn, findsOneWidget);
+    //Verify that the Sign Up button is displayed
+    final signupBtn = find.byKey(const Key('signupBtn'));
+    expect(signupBtn, findsOneWidget);
 
-    // Tap the Login button
-    await tester.tap(loginBtn);
+    // Tap the Sign Up button
+    await tester.tap(signupBtn);
     await tester.pump();
 
-    // Verify that the login button was pressed by checking for an action
+    // Verify that the sign up button was pressed by checking for an action
     expect(find.byType(SnackBar), findsOneWidget);
+    expect(find.text('Sign up Button tapped!'), findsOneWidget);
   });
 }
