3c3bec6889a5dee3a7bc60f92219cf3f52ffa598
Herbi3333 <hermat3333@gmail.com>
2023-03-06 13:57:05 -0700

    Update signUpBtn_test.dart

--- test/signUpBtn_test.dart
+++ test/signUpBtn_test.dart
@@ -1,38 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:cfmkids/main.dart';
-
-// TODO: Add test cases for Sign up button
-// TODO: Add test cases to see if images displayed correctly
-// TODO: Add test cases to see if the layout is centered and correctly displaying padding and spacing
-// TODO: Add test cases to verify colors and texts for buttons
-// TODO: Add test cases to verify the app against different screen sizes and orientations
+import 'package:cfmkids/mainpage.dart';
 
 void main() {
-  testWidgets('Sign Up button test', (WidgetTester tester) async {
-    // Build the widget
+  testWidgets('Button tap should trigger a function',
+      (WidgetTester tester) async {
+    // Build the widget tree
     await tester.pumpWidget(MaterialApp(
       home: HomePage(
-        body: signupBtn(
+        body: ElevatedButton(
           onPressed: () {
-            print('Sign up Button tapped!');
+            print('Button tapped!');
           },
           child: Text('Button'),
         ),
       ),
     ));
 
-    //Verify that the Sign Up button is displayed
-    final signupBtn = find.byKey(const Key('signupBtn'));
-    expect(signupBtn, findsOneWidget);
-
-    // Tap the Sign Up button
-    await tester.tap(signupBtn);
+    // Tap the button
+    await tester.tap(find.byType(ElevatedButton));
     await tester.pump();
 
-    // Verify that the sign up button was pressed by checking for an action
-    expect(find.byType(SnackBar), findsOneWidget);
-    expect(find.text('Sign up Button tapped!'), findsOneWidget);
+    // Verify that the function is called
+    expect(find.text('Button tapped!'), findsOneWidget);
   });
 }
